acbc592d80eb18d8bb88140ff1501e3d1b56e166
Romario Da Silva <roma.dasilva94@gmail.com>
2020-10-06 16:39:10 -0300

    Routes structure

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,12 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
 
 // Rota: Endereço completo da requisição
 // Recurso: Qual entidade estamos acessando do sistema
@@ -12,34 +18,4 @@ const app = express();
 
 // Request params: Parâmetros dentro da própria rota que identificam um recurso
 // Query params: Parâmetros dentro da própria rota, geralmente opcionais para filtros, paginação...
-
-const users = ["Diego", "Cleiton", "Robson", "Daniel"];
-
-app.get("/users", (request, response) => {
-  const search = String(request.query.search);
-
-  const filteredUsers = search
-    ? users.filter((user) => user.includes(search))
-    : users;
-
-  response.json(filteredUsers);
-});
-
-app.get("/users/:id", (request, response) => {
-  const id = Number(request.params.id);
-
-  const user = users[id];
-
-  return response.json(user);
-});
-
-app.post("/users", (request, response) => {
-  const user = {
-    name: "Diego",
-    email: "diego@rocketseat.com.br",
-  };
-
-  return response.json(user);
-});
-
-app.listen(3333);
+// Request body: Parâmetros para criação e atualização de info
